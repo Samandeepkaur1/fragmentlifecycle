ea05e615fb3267e397abbc24933c1245de237f72
Samandeep Kaur <109544923+Samandeepkaur1@users.noreply.github.com>
2022-08-04 10:48:19 +0530

    Initial commit

---
+++ app/src/main/java/com/saman/fragmentlifecycle/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.saman.fragmentlifecycle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         navController = findNavController(R.id.navHost)
+    }
+
+}
